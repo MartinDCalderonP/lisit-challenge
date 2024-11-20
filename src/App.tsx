cd91fc2cb4ce01e559c64244fe8b698471d1f5f4
Martín <martincalderon92@outlook.com>
2024-11-20 12:47:44 -0300

    Add routing for main application views in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,51 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { Navigate } from 'react-router-dom'
+
+const routes = [
+  {
+    path: '/',
+    element: <h1>Home</h1>
+  },
+  {
+    path: '/people',
+    element: <h1>People</h1>
+  },
+  {
+    path: '/people/:id',
+    element: <h1>Person</h1>
+  },
+  {
+    path: '/planets',
+    element: <h1>Planets</h1>
+  },
+  {
+    path: '/planets/:id',
+    element: <h1>Planet</h1>
+  },
+  {
+    path: '/starships',
+    element: <h1>Starships</h1>
+  },
+  {
+    path: '/starships/:id',
+    element: <h1>Starship</h1>
+  },
+  {
+    path: '*',
+    element: <Navigate to='/' />
+  }
+]
+
 const App = () => {
-  return <h1>Manosos</h1>
+  return (
+    <BrowserRouter>
+      <Routes>
+        {routes.map((route) => (
+          <Route key={route.path} path={route.path} element={route.element} />
+        ))}
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
 export default App
