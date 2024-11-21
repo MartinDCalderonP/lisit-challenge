fcb6d2f1089bf6b11f89a3d4baad86ca46244173
Martín <martincalderon92@outlook.com>
2024-11-21 06:56:58 -0300

    Add custom hook useGetEntityById for fetching entity data by ID

---
+++ src/hooks/useGetEntityById.tsx
@@ -0,0 +1,37 @@
+import { useEffect, useState } from 'react'
+import { Entity } from '../types/api'
+import { getEntityById } from '../services/api'
+
+interface UseGetEntityByIdProps {
+  endpoint: string
+  id: string
+}
+
+const useGetEntityById = ({ endpoint, id }: UseGetEntityByIdProps) => {
+  const [results, setResults] = useState<Entity>()
+  const [loading, setLoading] = useState(true)
+  const [error, setError] = useState('')
+
+  useEffect(() => {
+    const fetchPeople = async () => {
+      setLoading(true)
+
+      try {
+        const response = await getEntityById(endpoint, id)
+        setResults(response.data)
+      } catch (error) {
+        console.error('Error fetching:', error)
+        setError("Couldn't fetch data. Please try again later.")
+      }
+
+      setLoading(false)
+    }
+
+    const debounce = setTimeout(fetchPeople, 300)
+    return () => clearTimeout(debounce)
+  }, [endpoint, id])
+
+  return { results, loading, error }
+}
+
+export default useGetEntityById
