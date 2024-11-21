c8a151f5b09ffc23c90c660a2f4c11c9e1ce7d60
Martín <martincalderon92@outlook.com>
2024-11-21 06:37:07 -0300

    Import Navigate in a single statement from react-router-dom for cleaner code

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import { Navigate } from 'react-router-dom'
+import { BrowserRouter, Navigate, Routes, Route } from 'react-router-dom'
 import Layout from './components/Layout'
 import Home from './pages/Home'
 import People from './pages/People'
